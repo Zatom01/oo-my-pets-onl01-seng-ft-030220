686d3738acdb3328472ac1cb54dd3eb821c077e6
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-20 07:01:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,13 +69,12 @@ class Owner
   end 
   
   def sell_pets
-    
-    (self.dogs,self.cats).each do |pets|
-      pets.mood="nervous"
-    end 
+   all_pets = self.cats + self.dogs
+   all_pets.each do |pet|
+     pet.owner = nil
+     pet.mood = "nervous"
+   end
 
-  end 
-  
   def list_pets
     "I have #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end 
